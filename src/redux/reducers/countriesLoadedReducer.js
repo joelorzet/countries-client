e74981c1fa0b5079b66d5e40764cb39395ee093e
joelorzet <joelorzt@gmail.com>
2022-10-29 14:26:24 -0300

    refactor: improvements in some files

--- src/redux/reducers/countriesLoadedReducer.js
+++ src/redux/reducers/countriesLoadedReducer.js
@@ -1,9 +1,6 @@
 import {
 	GET_COUNTRIES,
-	SORT_A_TO_Z,
-	SORT_Z_TO_A,
-	SORT_BY_POPULATION_ASC,
-	SORT_BY_POPULATION_DES,
+	SORT_COUNTRIES,
 	FILTER_BY_ACTIVITY,
 	GET_COUNTRIES_BY_QUERY,
 	FILTER_BY_CONTINENT,
@@ -13,7 +10,7 @@ import {
 } from '../actions/actions.js';
 
 import getCountriesReduced from './reducingFunctions/getCountries.js';
-import { filterByActivity, filterByContintent } from './reducingFunctions/filterCountries.js';
+import filterContinents from './reducingFunctions/filterCountries.js';
 import sortCountries from './reducingFunctions/sortCountries.js';
 
 const initialState = {
@@ -36,23 +33,14 @@ export default function countriesLoadedReducer(state = initialState, action) {
 		case GET_COUNTRIES_BY_QUERY:
 			return { ...state, countriesFiltered: [...action.payload] };
 
-		case SORT_A_TO_Z:
-			return sortCountries(state, SORT_A_TO_Z);
-
-		case SORT_Z_TO_A:
-			return sortCountries(state, SORT_Z_TO_A);
-
-		case SORT_BY_POPULATION_ASC:
-			return sortCountries(state, SORT_BY_POPULATION_ASC);
-
-		case SORT_BY_POPULATION_DES:
-			return sortCountries(state, SORT_BY_POPULATION_DES);
+		case SORT_COUNTRIES:
+			return sortCountries(state, action.payload);
 
 		case FILTER_BY_CONTINENT:
-			return filterByContintent(state, action);
+			return filterContinents(state, action);
 
 		case FILTER_BY_ACTIVITY:
-			return filterByActivity(state, action);
+			return filterContinents(state, action);
 
 		case DEFAULT:
 			return {
